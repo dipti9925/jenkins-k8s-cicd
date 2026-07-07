f8b3d84fe179d0faf9139a60af9cc1a092cf6f8f
Dipti Badwaik <diptibadwaik28@gmail.com>
2026-07-07 20:07:14 +0530

    Test auto-deploy trigger

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from my Jenkins + Kubernetes CI/CD pipeline! Version 2\n');
+  res.end('Hello from my Jenkins + Kubernetes CI/CD pipeline! Version 3 - Auto Deployed!\n');
 });
 
 server.listen(port, () => console.log(`Running on port ${port}`));
