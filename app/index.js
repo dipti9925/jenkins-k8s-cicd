57106ddafa62abfc53ab4e7c7032d6eec83b302c
Dipti Badwaik <diptibadwaik28@gmail.com>
2026-07-07 17:57:57 +0530

    Update app message to v2

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from my Jenkins + Kubernetes CI/CD pipeline! Version 1\n');
+  res.end('Hello from my Jenkins + Kubernetes CI/CD pipeline! Version 2\n');
 });
 
 server.listen(port, () => console.log(`Running on port ${port}`));
